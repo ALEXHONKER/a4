2b66321b8999146325bcbac915abc87833e8ca83
alexhonker <alexhonker@gmail.com>
2015-11-22 00:44:30 -0500

    update

---
+++ tool2/test.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+int=25
+while(($int <= 50))
+do
+	i=0
+	sum=0
+	while(($i < 10))
+	do
+		echo ${int}_${i}
+		rep=0
+		./graphGen ${int} > file${int}
+		while(($rep < 10))
+		 do
+		  cat file${int} | ./a5-ece650 >> res${int}
+		  let "rep++"
+		 done
+		let "i++"
+	done	
+	let "int=int+5"
+done
